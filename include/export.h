7b52a648bdfb64b8d5550f9bba269bdb3dd5b50d
Thiago Macedo <1750447+macedot@users.noreply.github.com>
2026-03-03 20:47:37 +0100

    fix: correct include guard and avoid reserved identifiers
    - Fix copy-paste error in guard comment (__CPT_H__ -> GTS_CPT_EXPORT_H)
    - Use GTS_CPT_EXPORT_H (double underscore prefixes are reserved for implementations)

--- include/export.h
+++ include/export.h
@@ -1,8 +1,8 @@
-#ifndef __EXPORT_H__
-#define __EXPORT_H__
+#ifndef GTS_CPT_EXPORT_H
+#define GTS_CPT_EXPORT_H
 
 size_t cpt_eulerian_mesh_write(SignedDistance *pSignedDistance, FILE *fp);
 size_t cpt_eulerian_mesh_write_silo(SignedDistance *pSignedDistance, const char* szFileOut);
 size_t cpt_eulerian_mesh_write_ze(SignedDistance *pSignedDistance, GtsSurface *surface, const char* szFileOut);
 
-#endif // __CPT_H__
+#endif // GTS_CPT_EXPORT_H
